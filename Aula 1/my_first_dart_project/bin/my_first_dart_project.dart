81651f85e79b7e023c8fffb2a1e12def8d60a32c
fernando_santos <ferspaes@outlook.com>
2023-11-03 10:20:58 -0300

    Update my_first_dart_project.dart

--- Aula 1/my_first_dart_project/bin/my_first_dart_project.dart
+++ Aula 1/my_first_dart_project/bin/my_first_dart_project.dart
@@ -1,13 +1,20 @@
-
 void main(List<String> arguments) {
-  int idade = 0x000021;
+  int idade = 33;
   double altura = 1.81;
   bool geek = false;
-  String nome = "Fernando Paes";
-  String frase = "Meu nome e: $nome, "
-  "minha idade e: $idade, "
-  "minha altura e: $altura, "
-  "eu me considero geek? $geek";
+  const String nome = "Fernando Paes";
+  final String apelido;
+
+  apelido = 'Fer';
+
+  List<dynamic> fernando = [idade, altura, geek, nome, apelido];
+  String frase = "Meu nome e: ${fernando[3]}, "
+      "meu apelido e: ${fernando[4]}, "
+      "minha idade e: ${fernando[0]}, "
+      "minha altura e: ${fernando[1]}, "
+      "eu me considero geek? ${fernando[2]}";
+
+  List<String> listaNomes = ['Ricarth', 'Natalia', 'Alex', 'Andriu', 'Andre'];
 
   print(frase);
 }
